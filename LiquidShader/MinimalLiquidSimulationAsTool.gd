31f49a1d0f737fe12d6fd55bdcef57b0bb4a3959
niceAndGoodOnline <felix.ruttan@gmail.com>
2023-11-20 11:23:30 -0800

    fix non-tool version

--- LiquidShader/MinimalLiquidSimulationAsTool.gd
+++ LiquidShader/MinimalLiquidSimulationAsTool.gd
@@ -22,14 +22,14 @@ var coeff_old : Vector2
 var coeff_old_old : Vector2
 
 func _physics_process(delta):
-	var accell_3d:Vector3 = (pos3 - 2 * pos2 + pos1) * 3600
+	var accell_3d:Vector3 = (pos3 - 2 * pos2 + pos1) * 3600.0
 	pos1 = pos2
 	pos2 = pos3
 	pos3 = get_global_transform().origin + rotation
 	accell = Vector2(accell_3d.x + accell_3d.y, accell_3d.z + accell_3d.y)
 	coeff_old_old = coeff_old
 	coeff_old = coeff
-	coeff = (-springConstant * coeff_old - reaction * accell) + 2 * coeff_old - coeff_old_old - delta * dampening * (coeff_old - coeff_old_old) / 3600
+	coeff = (-springConstant * coeff_old - reaction * accell) / 3600.0 + 2 * coeff_old - coeff_old_old - delta * dampening * (coeff_old - coeff_old_old) 
 	liquidShaderMaterial.set_shader_parameter("coeff", coeff*liquid_mobility)
 	if (pos1.distance_to(pos3) < 0.01):
 		vel = clamp (vel-delta*1.0,0.0,1.0)
